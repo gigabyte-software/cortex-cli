3fb70e08a0d9d7c88c1e54724c384e4d18ae59c0
Rob Corley <rob@gigabyte.software>
2025-11-06 20:14:34 +0000

    Update verison number in Application.php

--- src/Application.php
+++ src/Application.php
@@ -42,7 +42,7 @@ class Application extends BaseApplication
 
     public function __construct()
     {
-        parent::__construct('Cortex CLI', '1.0.0');
+        parent::__construct('Cortex CLI', '1.0.4');
 
         // Simple dependency injection
         $configValidator = new ConfigValidator();
